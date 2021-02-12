956f845778bb649980b6bb7690237404c9e917c8
anuragpatidar17 <anurag.patidar@yahoo.in>
2021-02-12 18:31:02 +0530

    inital

---
+++ yolo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace yolo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driver = new ChromeDriver();
+
+
+
+            driver.Navigate().GoToUrl("http://www.google.com/");
+
+
+            IWebElement query = driver.FindElement(By.Name("q"));
+
+
+            Console.WriteLine(driver.Title + " Checking");
+
+
+            query.Submit();
+
+
+
+            driver.Quit();
+
+        }
+    }
+
+}
